9be8785360af34dec0cf6019b77d571f4fbdabdc
avazurita <ava.zurita@gmail.com>
2021-05-07 10:41:14 -0400

    Fixed binding and indentation.

--- FickleWriter/app/src/main/java/com/zurita/ficklewriter/ui/main/EditorFragment.kt
+++ FickleWriter/app/src/main/java/com/zurita/ficklewriter/ui/main/EditorFragment.kt
@@ -8,42 +8,44 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.zurita.ficklewriter.R
-import com.zurita.ficklewriter.databinding.MainFragmentBinding
+import com.zurita.ficklewriter.databinding.EditorFragmentBinding
 
 class EditorFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = EditorFragment()
-    }
+   companion object {
+      fun newInstance() = EditorFragment()
+   }
 
-    private var _binding: MainFragmentBinding? = null
+   private var _binding: EditorFragmentBinding? = null
 
-    /**
-     * This property is only valid between onCreateView and
-     * onDestroyView.
-     */
-    private val binding get() = _binding!!
+   /**
+    * This property is only valid between onCreateView and
+    * onDestroyView.
+    */
+   private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        _binding = MainFragmentBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+   override fun onCreateView(
+      inflater: LayoutInflater, container: ViewGroup?,
+      savedInstanceState: Bundle?
+   ): View {
+      _binding = EditorFragmentBinding.inflate(inflater, container, false)
+      return binding.root
+   }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.toolbar.inflateMenu(R.menu.toolbar)
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+      super.onViewCreated(view, savedInstanceState)
+      binding.toolbar.inflateMenu(R.menu.toolbar)
 
-        val model by activityViewModels<EditorViewModel>()
+      val model by activityViewModels<EditorViewModel>()
 
-        // Save the KeyListener by putting it into the tag of its textbox,
-        // then continue to set it based on the model. Setting the KeyListener
-        // to null will make the textbox not editable
-        binding.editor.textbox.tag = binding.editor.textbox.keyListener
-        model.editModeEnabled.observe(this, { editModeEnabled ->
-            binding.editor.textbox.keyListener =
-                if(editModeEnabled) binding.editor.textbox.tag as KeyListener
-                else null
-        })
-    }
+      // Save the KeyListener by putting it into the tag of its textbox,
+      // then continue to set it based on the model. Setting the KeyListener
+      // to null will make the textbox not editable
+      binding.editor.textbox.tag = binding.editor.textbox.keyListener
+      model.editModeEnabled.observe(this, { editModeEnabled ->
+         binding.editor.textbox.keyListener =
+            if (editModeEnabled) binding.editor.textbox.tag as KeyListener
+            else null
+      })
+   }
 }
--- FickleWriter/app/src/main/java/com/zurita/ficklewriter/ui/main/EditorViewModel.kt
+++ FickleWriter/app/src/main/java/com/zurita/ficklewriter/ui/main/EditorViewModel.kt
@@ -5,21 +5,21 @@ import androidx.lifecycle.ViewModel
 
 class EditorViewModel : ViewModel() {
 
-    // Backing fields used internally
-    private val _editModeEnabled = MutableLiveData(false)
+   // Backing fields used internally
+   private val _editModeEnabled = MutableLiveData(false)
 
-    /**
-     *  The view can be either in edit mode or normal mode.
-     *  A value of true means edit mode. In edit mode the user
-     *  can change the title, description, text, etc.
-     */
-    val editModeEnabled = _editModeEnabled
+   /**
+    *  The view can be either in edit mode or normal mode.
+    *  A value of true means edit mode. In edit mode the user
+    *  can change the title, description, text, etc.
+    */
+   val editModeEnabled = _editModeEnabled
 
-    /**
-     * Toggle between edit mode and normal mode. This will trigger
-     * a live data update.
-     */
-    fun toggleEditMode() {
-        _editModeEnabled.postValue(!_editModeEnabled.value!!)
-    }
+   /**
+    * Toggle between edit mode and normal mode. This will trigger
+    * a live data update.
+    */
+   fun toggleEditMode() {
+      _editModeEnabled.postValue(!_editModeEnabled.value!!)
+   }
 }
