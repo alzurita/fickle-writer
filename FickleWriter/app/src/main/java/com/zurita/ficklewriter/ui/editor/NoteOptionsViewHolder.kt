18d823cb915ab138348d814080b6bfffb2d90b1f
alzurita <ava.zurita@gmail.com>
2021-05-21 18:37:33 -0400

    Use resource for pin and unpin string.

--- FickleWriter/app/src/main/java/com/zurita/ficklewriter/ui/editor/NoteOptionsViewHolder.kt
+++ FickleWriter/app/src/main/java/com/zurita/ficklewriter/ui/editor/NoteOptionsViewHolder.kt
@@ -2,6 +2,7 @@ package com.zurita.ficklewriter.ui.editor
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.zurita.ficklewriter.R
 import com.zurita.ficklewriter.databinding.NoteOptionsBinding
 
 class NoteOptionsViewHolder(
@@ -14,11 +15,14 @@ class NoteOptionsViewHolder(
 
    private var note = Note()
 
+   private val textPin = itemView.context.resources.getString(R.string.pin)
+   private val textUnpin = itemView.context.resources.getString(R.string.unpin)
+
    init
    {
       binding.pin.setOnClickListener {
          note.pinned = !note.pinned
-         binding.pin.text = if(note.pinned) "Unpin" else "Pin"
+         setPinnedText()
          onPinSelected(note)
       }
    }
@@ -26,6 +30,11 @@ class NoteOptionsViewHolder(
    override fun bind(item: Note)
    {
       note = item
-      binding.pin.text = if(note.pinned) "Unpin" else "Pin"
+      setPinnedText()
+   }
+
+   private fun setPinnedText()
+   {
+      binding.pin.text = if(note.pinned) textUnpin else textPin
    }
 }
