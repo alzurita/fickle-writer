7cc092b11c9915fe38aca0c3e4a3dee744fe3798
avazurita <ava.zurita@gmail.com>
2021-06-06 10:25:35 -0400

    Renamed Editor fragments to Main fragments.

--- FickleWriter/app/src/main/java/com/zurita/ficklewriter/ui/main/MainFragment.kt
+++ FickleWriter/app/src/main/java/com/zurita/ficklewriter/ui/main/MainFragment.kt
@@ -1,29 +1,27 @@
 package com.zurita.ficklewriter.ui.main
 
 import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.zurita.ficklewriter.R
-import com.zurita.ficklewriter.databinding.EditorFragmentBinding
+import com.zurita.ficklewriter.databinding.MainFragmentBinding
 import com.zurita.ficklewriter.data.Note
 import com.zurita.ficklewriter.model.NotesContainerViewModel
 import com.zurita.ficklewriter.ui.editor.EditorAdapter
 import com.zurita.ficklewriter.ui.editor.viewholder.NoteOptionsViewHolderListener
 import com.zurita.ficklewriter.ui.main.CustomIntent.DATA_NOTE
 
-class EditorFragment
+class MainFragment
    : Fragment(),
      NoteOptionsViewHolderListener
 {
-   private var _binding: EditorFragmentBinding? = null
+   private var _binding: MainFragmentBinding? = null
    /** Editor binding to interact with underlying View
     * Only valid between onCreateView and onDestroyView. */
    private val binding get() = _binding!!
@@ -34,7 +32,7 @@ class EditorFragment
       savedInstanceState: Bundle?
    ): View
    {
-      _binding = EditorFragmentBinding.inflate(inflater, container, false)
+      _binding = MainFragmentBinding.inflate(inflater, container, false)
       return binding.root
    }
 
@@ -74,7 +72,7 @@ class EditorFragment
       val navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
       if(navController.currentDestination?.id == R.id.editorFragment)
       {
-         val directions = EditorFragmentDirections.actionEditorFragmentToNotesContainerFragment()
+         val directions = MainFragmentDirections.actionEditorFragmentToNotesContainerFragment()
          navController.navigate(directions)
       }
    }
